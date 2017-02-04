0ea2c3656418e2af6da1e74ff631fb2bf657841c
Byron Ajin <Byron Ajin>
2017-02-03 22:12:09 -0600

    Step 3 - Log mesages

--- app/src/main/java/com/beginner/micromaster/contactlist/MainActivity.java
+++ app/src/main/java/com/beginner/micromaster/contactlist/MainActivity.java
@@ -2,12 +2,14 @@ package com.beginner.micromaster.contactlist;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
+    private static final String TAG = MainActivity.class.getSimpleName();
     private EditText nameEditText;
     private EditText lastNameEditText;
     private EditText emailEditText;
@@ -29,7 +31,7 @@ public class MainActivity extends AppCompatActivity {
 
             public void onClick(View v) {
                 String name = nameEditText.getText().toString();
-                String lastName = lastNameEditText.getText().toString();
+                String lastName = nameEditText.getText().toString();
                 String email = emailEditText.getText().toString();
                 String phoneNumber = phoneNumberEditText.getText().toString();
 
@@ -44,6 +46,7 @@ public class MainActivity extends AppCompatActivity {
 
     private void createNewContact(String name, String lastName, String email, Integer phoneNumber) {
         Contact contact = new Contact(name, lastName, email, phoneNumber);
+        Log.d(TAG, "New contact created: " + contact.toString());
         clearEditText();
     }
 
