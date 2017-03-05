70723437d7a611e9a22fb3e7a15c1a23af52d25c
Byron <byronap120@gmail.com>
2017-03-04 21:00:28 -0600

    Fixing Master branch

--- app/src/main/java/com/beginner/micromaster/contactlist/Contact.java
+++ app/src/main/java/com/beginner/micromaster/contactlist/Contact.java
@@ -1,8 +1,25 @@
 package com.beginner.micromaster.contactlist;
 
-/**
- * Created by Byron on 3/4/2017.
- */
-
 public class Contact {
+
+    private String name;
+    private String lastName;
+    private String email;
+    private int phoneNumber;
+
+    public Contact(String name, String lastName, String email, int phoneNumber) {
+        this.name = name;
+        this.lastName = lastName;
+        this.email = email;
+        this.phoneNumber = phoneNumber;
+    }
+
+    @Override
+    public String toString() {
+        return "name:" + name + ", " +
+                "lastName:" + lastName + ", " +
+                "email:" + email + ", " +
+                "phoneNumber:" + phoneNumber;
+
+    }
 }
