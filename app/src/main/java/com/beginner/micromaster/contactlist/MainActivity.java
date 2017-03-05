b0acbe52c924fc15b1cfb3231f29c5673d8ca855
Byron <byronap120@gmail.com>
2017-03-04 20:54:16 -0600

    Revert changes for listview

--- app/src/main/java/com/beginner/micromaster/contactlist/MainActivity.java
+++ app/src/main/java/com/beginner/micromaster/contactlist/MainActivity.java
@@ -3,49 +3,74 @@ package com.beginner.micromaster.contactlist;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
-import android.widget.ListView;
-
-import com.beginner.micromaster.contactlist.adapters.ContactListAdapter;
-import com.beginner.micromaster.contactlist.models.Contact;
-
-import java.util.ArrayList;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
-    private ContactListAdapter adapter;
-    private ArrayList<Contact> arrayOfUsers;
+    private static final String TAG = MainActivity.class.getSimpleName();
+    private EditText nameEditText;
+    private EditText lastNameEditText;
+    private EditText emailEditText;
+    private EditText phoneNumberEditText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        arrayOfUsers = getContactsFromDB();
-        adapter = new ContactListAdapter(this, arrayOfUsers);
+        //Get EditText references
+        nameEditText = (EditText) findViewById(R.id.input_name);
+        lastNameEditText = (EditText) findViewById(R.id.input_lastName);
+        emailEditText = (EditText) findViewById(R.id.input_email);
+        phoneNumberEditText = (EditText) findViewById(R.id.input_phoneNumber);
 
-        ListView listView = (ListView) findViewById(R.id.contact_list_view);
-        listView.setAdapter(adapter);
-
-        final Button button = (Button) findViewById(R.id.add_new_contact);
+        Button button = (Button) findViewById(R.id.button_create_contact);
         button.setOnClickListener(new View.OnClickListener() {
+
             public void onClick(View v) {
-                Intent downloadIntent = new Intent(getApplicationContext(), AddNewContactActivity.class);
-                startActivity(downloadIntent);
+                String name = nameEditText.getText().toString();
+                String lastName = lastNameEditText.getText().toString();
+                String email = emailEditText.getText().toString();
+                String phoneNumber = phoneNumberEditText.getText().toString();
+
+                //Get values from EditText and create a new Contact
+                Integer phoneNumberInt = parsePhoneNumber(phoneNumber);
+                if (phoneNumberInt != null) {
+                    createNewContact(name, lastName, email, phoneNumberInt);
+                }
             }
         });
     }
 
-    @Override
-    protected void onResume() {
-        super.onResume();
-        arrayOfUsers = getContactsFromDB();
-        adapter.notifyDataSetChanged();
+    private void createNewContact(String name, String lastName, String email, Integer phoneNumber) {
+        Contact contact = new Contact(name, lastName, email, phoneNumber);
+        Log.d(TAG, "New contact created: " + contact.toString());
+        clearEditText();
+
+        Intent intent = new Intent(this, DetailActivity.class);
+        //TODO: send the rest of contact parameters
+        intent.putExtra("name", name);
+        intent.putExtra("lastName", lastName);
+        intent.putExtra("email", email);
+        intent.putExtra("phoneNumber", phoneNumber);
+        startActivity(intent);
     }
 
-    private ArrayList<Contact> getContactsFromDB() {
-        return new ArrayList<>(Contact.getAll());
+    private void clearEditText() {
+        nameEditText.setText("");
+        lastNameEditText.setText("");
+        emailEditText.setText("");
+        phoneNumberEditText.setText("");
     }
 
+    public static Integer parsePhoneNumber(String phoneNumber) {
+        try {
+            return Integer.parseInt(phoneNumber);
+        } catch (NumberFormatException e) {
+            return null;
+        }
+    }
 }
